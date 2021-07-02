54fba9fb3ad9f51527d3480dd069011fd8626f6c
hayashitoshiki <tosikihayasi@outlook.jp>
2021-07-02 21:09:42 +0900

    first commit

---
+++ ViperSampleIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperSampleIOS
+//
+//  Created by 林敏生 on 2021/07/02.
+//  Copyright © 2021 林敏生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViperSampleIOSTests/ViperSampleIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViperSampleIOSTests.swift
+//  ViperSampleIOSTests
+//
+//  Created by 林敏生 on 2021/07/02.
+//  Copyright © 2021 林敏生. All rights reserved.
+//
+
+import XCTest
+@testable import ViperSampleIOS
+
+class ViperSampleIOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
